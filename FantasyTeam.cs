0c2c34c70b8ce019649a0ae1695450406fe780eb
Fred Clay <fred.clay1977@gmail.com>
2022-07-20 13:51:59 -0400

    Team updates

---
+++ FantasyTeam.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+
+
+public class FantasyTeam 
+{
+    
+    public string? QB1 {get; set;}
+
+    public string? QB2 {get; set;}
+
+    public string? RB1 {get; set;}
+
+    public string? RB2 {get; set;}
+
+    public string? WR1 {get; set;}
+
+    public string? WR2 {get; set;}
+
+    public string? TE {get; set;}
+
+    public string? Flex {get; set;}
+
+    public string? Kicker {get;set;}
+
+    public string? Defense {get;set}
+
+}
