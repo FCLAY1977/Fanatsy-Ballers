bbd6e7ac12399d3be41691cab3c88e1742e4ab16
Fred Clay <fred.clay1977@gmail.com>
2022-07-26 17:32:58 -0400

    Update migration

---
+++ Migrations/20220726213216_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FantasyBallers.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FantasyTeams",
+                table: "FantasyTeams");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QB1",
+                table: "FantasyTeams",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ID",
+                table: "FantasyTeams",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FantasyTeams",
+                table: "FantasyTeams",
+                column: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FantasyTeams",
+                table: "FantasyTeams");
+
+            migrationBuilder.DropColumn(
+                name: "ID",
+                table: "FantasyTeams");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QB1",
+                table: "FantasyTeams",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FantasyTeams",
+                table: "FantasyTeams",
+                column: "QB1");
+        }
+    }
+}
